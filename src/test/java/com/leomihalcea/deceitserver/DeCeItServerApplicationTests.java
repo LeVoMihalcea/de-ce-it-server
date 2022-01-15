a586b56b03460e9b057b8f67c997ec4f3612cf45
Leonard Mihalcea <Leonard.Mihalcea@cloudflight.io>
2022-01-15 16:05:17 +0200

    init

---
+++ src/test/java/com/leomihalcea/deceitserver/DeCeItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leomihalcea.deceitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeCeItServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
